cc73d3cb7a92df3d70529c767c8a090fec74adee
Arqam Rafay <arqam.rafay@net.sixlogs.com>
2021-12-10 10:52:40 +0500

    initial commit

---
+++ app/models/tutorial.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, Sequelize) => {
+  const Tutorial = sequelize.define("tutorial", {
+    title: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.STRING
+    },
+    published: {
+      type: Sequelize.BOOLEAN
+    }
+  });
+
+  // `sequelize.define` also returns the model
+  // console.log(Tutorial === sequelize.models.Tutorial); // true
+
+  return Tutorial;
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+// app.use(...)
+const db = require("./app/models");
+db.sequelize.sync();
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("All models were synchronized successfully.");
+    console.log("Drop and re-sync db.");
+  });
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+// simple route
+app.get("/", (req, res) => {
+    return res.json({ message: "Welcome to bezkoder application." });
+});
+
+// require("./app/routes/turorial.routes")(app);
+require("./app/routes/tutorial.routes")(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
