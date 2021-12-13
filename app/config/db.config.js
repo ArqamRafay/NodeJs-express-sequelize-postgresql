a4a862c2b9f7dd2835c945067cf2f4391e09ebf4
Arqam Rafay <arqam.rafay@net.sixlogs.com>
2021-12-13 15:53:09 +0500

    authentication apply only, using jwt

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "Admin@123",
+    PASSWORD: "admin",
     DB: "dbCompany",
     dialect: "postgres",
     pool: {
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const jwt = require("jsonwebtoken")
+var expressJWT = require("express-jwt")
 const app = express();
-// app.use(...)
 const db = require("./app/models");
 // db.sequelize.sync();
-
 db.sequelize.sync().then(() => {
     console.log("All models were synchronized successfully.");
     console.log("Drop and re-sync db.");
 }, { logging: false });
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-};
-
+var corsOptions = { origin: "http://localhost:8081" };
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
@@ -24,21 +20,67 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+//SECRET FOR JSON WEB TOKEN
+let secret = 'some_secret';
 
-// simple route
-app.get("/", (req, res) => {
-    return res.json({ message: "Welcome to bezkoder application." });
+/* ALLOW PATHS WITHOUT TOKEN AUTHENTICATION */
+app.use(expressJWT({ secret: secret, algorithms: ['HS256'] }).unless(
+    {
+        path: [
+            '/token/createNewUser',
+            '/',
+        ]
+    }
+))
+
+function generateAccessToken(userData) {
+    return jwt.sign(userData, secret, { expiresIn: '1800s' })   // 1800 (30 minutes)
+}
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, secret, (err, user) => {
+        console.log(err)
+
+        if (err) return res.sendStatus(403)
+        console.log(user)
+        req.user = user
+
+        next()
+    })
+}
+// Generating a Token
+app.get('/token/createNewUser', (req, res) => {
+    var userData = {
+        "name": "Muhammad Bilal",
+        "id": "4321"
+    }
+    const token = generateAccessToken(userData)
+    return res.status(200).json({ "token": token });
+})
+
+// Authenticating a Token
+app.get('/token/userOrders', authenticateToken, (req, res) => {
+    return res.status(200).json({
+        "success": true,
+        "msg": "Secrect Access Granted"
+    });
 });
 
-// require("./app/routes/turorial.routes")(app);
+// Public route
+app.get("/", (req, res) => {
+    return res.json({ message: "Welcome to application." });
+});
+
 require("./app/routes/tutorial.routes")(app);
 require("./app/routes/article.routes")(app);
 
-
 // set port, listen for requests
 const PORT = process.env.PORT || 8081;
-
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
